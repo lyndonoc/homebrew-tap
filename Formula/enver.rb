510844c1cc05a8d95eb003bb90a8e18e94668482
Lyndon Ohhyung Chun <lyndonoc@gmail.com>
2023-04-21 11:43:13 -0400

    enver 0.1.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/enver.rb
+++ Formula/enver.rb
@@ -1,9 +1,9 @@
 class Enver < Formula
   desc "A rust CLI that allows you to run an arbitrary command with a given set of environment variables"
   homepage "https://github.com/lyndonoc/enver"
-  url "https://github.com//lyndonoc/enver/releases/download/v0.1.0/enver-v0.1.0-universal-apple-darwin.tar.gz"
-  sha256 "bc0ac9699834e8e2f54b6ff6d22a970c84309b32d0a6126721422ea619a5c865"
-  version "0.0.1"
+  url "https://github.com/lyndonoc/enver/releases/download/v0.1.0/enver-v0.1.0-universal-apple-darwin.tar.gz"
+  sha256 "dc3eaa5b731578cce16abe4ad4780639b10cc54f22138351be9eae5cc5ee73a3"
+  version "0.1.0"
 
   def install
     bin.install "enver"
